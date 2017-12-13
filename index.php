575e4b2233105a34a620a75fd1d07dd662383a85
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 10:14:11 +0100

    Création de l'exercice 4

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 4</title>
+</head>
+<body>
+  <?php
+  for ($number=1; $number <= 10; $number+=$number/2) {
+    echo $number . ' ';
+  }
+  ?>
+</body>
+</html>
